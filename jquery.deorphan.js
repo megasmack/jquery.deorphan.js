0fd623c0ab4268aa9dcb321d777a3c3df588703c
Steve Schrab <steve.schrab@gsdesign.com>
2015-08-28 13:54:23 -0500

    Better jshint compatibility

--- jquery.deorphan.js
+++ jquery.deorphan.js
@@ -1,10 +1,10 @@
 /*
- *  deOrphan 1.1.1 - jQuery Plugin
+ *  deOrphan 1.1.3 - jQuery Plugin
  *  https://github.com/megasmack/jquery.deorphan.js
  *  written by Steve Schrab - @megasmack
  *  Special thanks to Erik Wessel - @wesselej and Jeffrey Schrab - @jschrab
  *
- *  Copyright (c) 2012 Steve Schrab (http://www.gsdesign.com/)
+ *  Copyright (c) 2015 Steve Schrab (http://www.gsdesign.com/)
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/mit-license.php
  *
@@ -16,28 +16,12 @@
  *
  *  http://en.wikipedia.org/wiki/Widows_and_orphans
  */
-
- /*
-    To activate the plugin add the following code to your own js file.
-
-        $('.your-class-name').deOrphan();
-
-    You can provide a list of elements you want deOrphan'd as well.
-
-        $('.your-class-name, .another-class-name, p, section p').deOrphan();
-
-    If you put your JavaScript in the head of your document, make sure your DOM is ready.
-
-        $().ready(function() {
-            $('.your-class-name').deOrphan();
-        });
- */
 (function($){
     $.fn.deOrphan = function() {
 
         function _deOrphan(node) {
             $(node).contents().each(function (i,node) {
-                if (node.nodeType == 3 && (typeof(node.textContent) !== 'undefined')) { // nodeType 3 is TEXT_NODE
+                if (node.nodeType === 3 && (typeof(node.textContent) !== 'undefined')) { // nodeType 3 is TEXT_NODE
                     node.textContent = node.nodeValue.replace(/ (\S*)$/, '\u00A0$1');
                 } else {
                     _deOrphan(node);
